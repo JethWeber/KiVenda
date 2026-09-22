4158a630d52214ad13a60f19718341023d21fcba
Jeth Weber <jethweber@gmail.com>
2026-09-22 20:12:01 +0100

    fase10: adicionar code-behind da vista de auditoria

---
+++ src/KiVenda.Desktop/Views/Modulos/AuditoriaView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KiVenda.Desktop.Views.Modulos;
+
+public partial class AuditoriaView : UserControl
+{
+    public AuditoriaView()
+    {
+        InitializeComponent();
+    }
+}
