199ea86ad0995e9ecf3847bbd8db31e840ef7745
Jeth Weber <jethweber@gmail.com>
2026-09-22 19:40:28 +0100

    fase9: adicionar code-behind dos relatórios

---
+++ src/KiVenda.Desktop/Views/Modulos/RelatoriosView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KiVenda.Desktop.Views.Modulos;
+
+public partial class RelatoriosView : UserControl
+{
+    public RelatoriosView()
+    {
+        InitializeComponent();
+    }
+}
