b4144c71be206425b118d9c24582f5ef04a5b8b7
Jeth Weber <jethweber@gmail.com>
2026-09-22 19:26:02 +0100

    Fase 8 Parte 4: adicionar configuração do scanner

---
+++ src/KiVenda.Desktop/Views/Modulos/ConfiguracaoScannerView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace KiVenda.Desktop.Views.Modulos;
+
+public partial class ConfiguracaoScannerView : UserControl
+{
+    public ConfiguracaoScannerView() => InitializeComponent();
+}
