470a9660e2a398f62ad1238fc48f2bc737bb9f7e
Jeth Weber <jethweber@gmail.com>
2026-09-22 22:41:27 +0100

    feat: expose backup in settings view model

--- src/KiVenda.Desktop/ViewModels/Modulos/ConfiguracoesViewModel.cs
+++ src/KiVenda.Desktop/ViewModels/Modulos/ConfiguracoesViewModel.cs
@@ -1,7 +1,15 @@
+using KiVenda.Infrastructure.Backup;
+
 namespace KiVenda.Desktop.ViewModels.Modulos;
 
 public sealed class ConfiguracoesViewModel : ViewModelBase
 {
     public ConfiguracaoScannerViewModel Scanner { get; } = new();
     public ConfiguracaoLicencaViewModel Licenca { get; } = new();
+    public ConfiguracaoBackupViewModel Backup { get; }
+
+    public ConfiguracoesViewModel(IServicoBackup servicoBackup)
+    {
+        Backup = new ConfiguracaoBackupViewModel(servicoBackup);
+    }
 }
