254fc8b4b52b8b47b4e8633289893ae9120326c1
Jeth Weber <jethweber@gmail.com>
2026-09-25 22:20:26 +0100

    feat: adicionar code-behind da configuração da impressora

---
+++ src/KiVenda.Desktop/Views/Modulos/ConfiguracaoImpressoraView.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace KiVenda.Desktop.Views.Modulos;
+
+public partial class ConfiguracaoImpressoraView : UserControl
+{
+    public ConfiguracaoImpressoraView() => InitializeComponent();
+}
